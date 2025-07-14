dc5c2b888b4606851a11d73089d6b7dda2c70343
ayushsareen <ayushsareen13@gmail.com>
2025-07-14 18:53:39 +0530

    added render site

--- frontend/src/Whiteboard.jsx
+++ frontend/src/Whiteboard.jsx
@@ -2,7 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import * as fabric from "fabric";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+// const socket = io("http://localhost:5000");
+const socket = io("https://colaborative-whiteboard.onrender.com");
 const CANVAS_BG_COLOR = "#ffffff";
 
 function Whiteboard({ roomId }) {
